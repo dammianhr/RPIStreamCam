0269923914b7a26d9dc9d7ad9fb5a48c3edbc681
damian nahmiyas <damm.hr@hotmail.com>
2016-01-30 11:22:48 -0300

    TESTED ON RASPBERRY PI B+ RASPBIAN JESSIE OF 090
    Requires the following addons:
    https://github.com/bakercp/ofxHTTP
    https://github.com/jvcleave/ofxRPiCameraVideoGrabber

---
+++ src/main.cpp
@@ -0,0 +1,108 @@
+
+#include "ofMain.h"
+#include "ofxHTTP.h"
+//#include "ofxRPiCameraVideoGrabber.h"
+#include "RPiVideoGrabber.h"
+#include "ofAppEGLWindow.h"
+
+class ofApp : public ofBaseApp{
+    ofx::HTTP::SimpleIPVideoServer server;
+    //ofxRPiCameraVideoGrabber videoGrabber;
+    RPiVideoGrabber vidGrabber;
+    int camWidth;
+    int camHeight;
+    OMXCameraSettings omxCameraSettings;
+    //ofPixels pix;
+    
+public:
+    //--------------------------------------------------------------
+    void setup(){
+        ofSetLogLevel(OF_LOG_VERBOSE);
+        ofSetLogLevel("ofThread", OF_LOG_ERROR);
+        /*
+        omxCameraSettings.width = 320;
+        omxCameraSettings.height = 240;
+        omxCameraSettings.framerate = 15;
+        omxCameraSettings.isUsingTexture = true;
+        
+        videoGrabber.setup(omxCameraSettings);
+        videoGrabber.enablePixels();
+        */
+        
+        camWidth = 320;
+        camHeight = 240;
+        vidGrabber.setDesiredFrameRate(15);
+        vidGrabber.initGrabber(camWidth, camHeight);
+        
+        ofx::HTTP::SimpleIPVideoServerSettings settings;
+        
+        settings.setPort(7890);
+        settings.ipVideoRouteSettings.setMaxClientConnections(1);
+        server.setup(settings);
+        
+        server.start();
+    }
+    
+    //--------------------------------------------------------------
+    void update(){
+        vidGrabber.update();
+        if (vidGrabber.isFrameNew()){
+            ofPixels& pixels = vidGrabber.getPixels();
+            server.send(pixels);
+        }
+        
+    }
+    
+    //--------------------------------------------------------------
+    void draw(){
+        //videoGrabber.draw();
+        vidGrabber.draw(0, 0);
+        stringstream info;
+        info << "App FPS: " << ofGetFrameRate() << "\n";
+        info << "URL: " << server.getURL() << "\n";
+        info << "Camera Resolution: " << vidGrabber.getWidth() << "x" << vidGrabber.getHeight()	<< " @ "<< vidGrabber.getFrameRate() <<"FPS"<< "\n";
+        
+        ofDrawBitmapStringHighlight(info.str(), 0, vidGrabber.getHeight(), ofColor::black, ofColor::yellow);
+    }
+};
+
+#if (OF_VERSION_MINOR != 9) && defined(TARGET_OPENGLES)
+#include "ofGLProgrammableRenderer.h"
+#endif
+
+#define FORCE_PROGRAMMMABLE 1
+
+#ifdef FORCE_PROGRAMMMABLE
+#include "ofGLProgrammableRenderer.h"
+#endif
+int main()
+{
+    ofSetLogLevel(OF_LOG_VERBOSE);
+#if defined(TARGET_OPENGLES)
+#if (OF_VERSION_MINOR == 9)
+    ofGLESWindowSettings settings;
+    settings.width = 480;
+    settings.height = 320;
+    settings.setGLESVersion(2);
+    ofCreateWindow(settings);
+#else
+    ofSetLogLevel("ofThread", OF_LOG_ERROR);
+    ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
+    ofSetupOpenGL(480, 320, OF_WINDOW);
+#endif
+#else
+#ifdef FORCE_PROGRAMMMABLE
+    ofGLWindowSettings glWindowSettings;
+    glWindowSettings.width = 480;
+    glWindowSettings.height = 320;
+    glWindowSettings.setGLVersion(3, 2);
+    ofCreateWindow(glWindowSettings);
+#else
+    ofSetLogLevel("ofThread", OF_LOG_ERROR);
+    ofSetupOpenGL(480, 320, OF_WINDOW);
+#endif
+    
+#endif
+    ofRunApp( new ofApp());
+}
+
